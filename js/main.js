8fb797378071599f35e41b546338617087b88e42
alamin <alaminhosen44000@gmail.com>
2023-08-10 08:55:06 +0600

    dev-2---v1.3

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
     $(".offcanvas-menu").addClass("active");
     $(".offcanvas-overlay").addClass("active");
   });
-  $(".offcanvas-menu a").click(function () {
+  $(".offcanvas-close").click(function () {
     $(".offcanvas-menu").removeClass("active");
     $(".offcanvas-overlay").removeClass("active");
   });
@@ -33,4 +33,63 @@ $(document).ready(function () {
 
   // preloader
   $("#preloader").fadeOut(500);
+});
+
+
+// slider-activation
+const swiper = new Swiper('.hero-slide-active', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 0,
+  speed: 1000,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+  },
+})
+
+  $('.swiper-pagination-bullet').hover(function() {
+    swiper.autoplay.stop();
+  },function() {
+      swiper.autoplay.start();
+  }); 
+
+// slider-activation
+const swiperPost = new Swiper('.post-row', {
+  // Default parameters
+  slidesPerView: 4,
+  spaceBetween: 24,
+  speed: 1000,
+  autoplay: {
+    delay: 4000,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 24
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 24
+    }
+  }
+})
+
+
+
+$(".post-row").hover(function() {
+    (this).swiper.autoplay.stop();
+}, function() {
+    (this).swiper.autoplay.start();
 });
